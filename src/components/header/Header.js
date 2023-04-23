8a4c5b6a9dede261d87a1218e9dc305c25ee9635
rudwnok <rudwnok123@naver.com>
2023-04-23 15:40:15 +0900

    로그인 | 회원가입 탭 선택

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import './header.scss';
-
-const HeaderStyle = styled.header`
-  color: red;
-`;
+import classNames from "classnames";
+import React, { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./header.scss";
 
 const Header = () => {
-  return (
-    <HeaderStyle>
-      <h1>
-        {/* NOTE: asdfasdf */}
-        {/* TODO: asdfasdf */}
-        {/* FIXME : asdfasdf */}
+  const location = useLocation();
 
+  useEffect(() => {
+    console.log(location.pathname);
+  }, [location]);
+
+  return (
+    <header>
+      <h1>
         <Link to="/">Developer-Talks</Link>
       </h1>
-      <Link to="/login">로그인 |</Link> <Link to="/regist">회원가입</Link>
-    </HeaderStyle>
+      <Link
+        to="/login"
+        className={classNames("", {
+          "is-active": location.pathname === "/login",
+        })}
+      >
+        로그인
+      </Link>
+      {" | "}
+      <Link
+        to="/regist"
+        className={classNames("", {
+          "is-active": location.pathname === "/regist",
+        })}
+      >
+        회원가입
+      </Link>
+    </header>
   );
 };
 export default Header;
