3ba2228555379a731fac9f433c9d3b95cc16e88c
ssing_7942 <djagmlwnn12@gmail.com>
2023-05-06 03:18:00 +0900

    부족하지만 여기까지 했어요 :-)

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,10 +2,13 @@ import { useEffect, useState } from 'react';
 import { FiMenu } from 'react-icons/fi';
 import { Link, useLocation } from 'react-router-dom';
 import './header.scss';
-import AlertModal from 'components/portalModal/AlertModal';
-import List from 'components/list/List';
-// import AlertModal from
+// import AlertModal from 'components/portalModal/AlertModal';
+// import { Children } from 'react';
+// import List from 'components/list/List';
 
+// const User = (user) => {
+//   user.title(user.amount);
+// };
 // alt + shiff + o
 const Header = () => {
   const [modalOpen, setModalOpen] = useState(false);
@@ -15,32 +18,29 @@ const Header = () => {
 
   const [header, setHeader] = useState('false');
 
-  const items = [
-    { id: 1, title: 'Item 1' },
-    { id: 2, title: 'Item 2' },
-    { id: 3, title: 'Item 3' },
-  ];
-
-  const [posts, setPosts] = useState([
+  let [user, setUser] = useState([
     {
-      title: '1',
+      id: '1',
       amount: 'test1',
       data: '사랑의 앞이 튼튼하며, 거친 사막이다. 청춘의 보배를 기쁘며, 날카로우나 구하지 하여도 그러므로 뿐이다. 이상 무엇을 목숨을 그들에게 천하를 능히 위하여, 그들은 듣기만 부패뿐이다. 내는 오직 실로 두손을 봄바람이다. 어디 무엇이 소금이라 있으며, 예가 기관과 인류의 뿐이다. 풀이 청춘의 지혜는 창공에 인간은 때까지 봄바람이다. 인류의 피는 주며, 자신과 쓸쓸하랴? 돋고, 그들의 것은 위하여, 그와 위하여서. 수 웅대한 설레는 피가 청춘이 피고, 것이다. 이는 이상이 구하기 생생하며, 천하를 운다.',
       nickname: 'Ann',
     },
     {
-      title: '2',
+      id: '2',
       amount: 'test2',
       data: 'bbbbbbbbbbbbbbb',
       nickname: 'Tree',
     },
     {
-      title: '3',
+      id: '3',
       amount: 'test3',
       data: 'ccccccccccccccccccccc',
       nickname: 'Lotto',
     },
   ]);
+
+  // const UserList = users.map((user) => <User />);
+
   // [읽기, 쓰기] = useState('초기값') // 초기값 타입 : string, number ,array, json, boolean(true, false)
 
   // return dom 그려질때. 추적하는 상태가 바뀔때.
@@ -73,12 +73,11 @@ const Header = () => {
 
           <li>
             <p onClick={showModal}>🔔</p>
-            {modalOpen && (
-              <AlertModal
-              // setModalOpen={setModalOpen}
-              // <List items={items}/>
-              />
-            )}
+            {modalOpen &&
+              user.map(function (a) {
+                return <div key={user.id}>{user.amount}</div>;
+              })}
+            {/* FIXME Key도 넣었는데 이게 왜 오류가 날까요? */}
           </li>
           <li>
             <Link to="/mypage">
--- src/pages/mypage/Mypage.js
+++ src/pages/mypage/Mypage.js
@@ -4,43 +4,50 @@ import { useSelector } from 'react-redux';
 import { Children, useState } from 'react';
 import './Mypage.scss';
 
-// import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation } from 'react-router-dom';
 
-const Mypage = () => {
-  // const location = useLocation();
+const Mypage = ({ user }) => {
+  const location = useLocation();
   const auth = useSelector((state) => state.authToken);
   console.log('auth:', auth);
 
   const contacts = [
-    { id: 0, type: '최근활동', line: '안녕' },
-    { id: 1, type: '내가 쓴 글', line: '안녕' },
-    { id: 2, type: '댓글', line: '안녕' },
-    { id: 3, type: '스크랩', line: '안녕' },
+    { id: 0, type: '최근활동', line: '최근 활동 리스트들' },
+    { id: 1, type: '내가 쓴 글', line: '최근 내가 쓴 글들' },
+    { id: 2, type: '댓글', line: '최근 댓글' },
+    { id: 3, type: '스크랩', line: '스크랩 한 글들' },
   ];
   const [select, setSelect] = useState('');
   const onSelect = (type) => {
     setSelect(type);
   };
 
-  return (
-    <main className="main">
-      {/* <Link
+  const LoginRegist = () => {
+    <>
+      <Link
         to="/login"
-        className={classNames("", {
-          "is-active": location.pathname === "/login",
+        className={classNames('', {
+          'is-active': location.pathname === '/login',
         })}
       >
         로그인
       </Link>
-      {" | "}
+      {' | '}
       <Link
         to="/regist"
-        className={classNames("", {
-          "is-active": location.pathname === "/regist",
+        className={classNames('', {
+          'is-active': location.pathname === '/regist',
         })}
       >
         회원가입
-      </Link> */}
+      </Link>
+    </>;
+  };
+
+  return (
+    <main className="main">
+      {{ user } ? '로그아웃' : <LoginRegist />}
+
       <Left />
 
       <section className="notes">
@@ -57,6 +64,7 @@ const Mypage = () => {
               <p className={`${select === contact.type ? 'selects' : ''}`}>
                 {contact.line}
               </p>
+              {/* FIXME 이건 버튼 클릭 시 색 변경까진 했는데, 클릭하고 내용이 나왔으면하는데, 모든 내용이 한 번에 다 나오네요..하다 보니 난리 났네요. 문제점 마구 알려주세요! */}
             </li>
           ))}
         </ul>
