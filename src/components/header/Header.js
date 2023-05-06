f92e8bc729ef83242e34755207669911a737d1d5
rudwnok123 <rudwnok123@naver.com>
2023-05-06 13:41:10 +0900

    수정

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react';
-import { FiMenu } from 'react-icons/fi';
-import { Link, useLocation } from 'react-router-dom';
-import './header.scss';
+import { useEffect, useState } from "react";
+import { FiMenu } from "react-icons/fi";
+import { AiFillBell } from "react-icons/ai";
+import { Link, useLocation } from "react-router-dom";
+import "./header.scss";
 // import AlertModal from 'components/portalModal/AlertModal';
 // import { Children } from 'react';
 // import List from 'components/list/List';
@@ -11,31 +12,30 @@ import './header.scss';
 // };
 // alt + shiff + o
 const Header = () => {
-  const [modalOpen, setModalOpen] = useState(false);
-  const showModal = () => {
-    setModalOpen(true);
+  const [popover, setPopover] = useState(false);
+  const [header, setHeader] = useState("false");
+  const showPopover = () => {
+    setPopover(!popover);
   };
-
-  const [header, setHeader] = useState('false');
 
   let [user, setUser] = useState([
     {
-      id: '1',
-      amount: 'test1',
-      data: '사랑의 앞이 튼튼하며, 거친 사막이다. 청춘의 보배를 기쁘며, 날카로우나 구하지 하여도 그러므로 뿐이다. 이상 무엇을 목숨을 그들에게 천하를 능히 위하여, 그들은 듣기만 부패뿐이다. 내는 오직 실로 두손을 봄바람이다. 어디 무엇이 소금이라 있으며, 예가 기관과 인류의 뿐이다. 풀이 청춘의 지혜는 창공에 인간은 때까지 봄바람이다. 인류의 피는 주며, 자신과 쓸쓸하랴? 돋고, 그들의 것은 위하여, 그와 위하여서. 수 웅대한 설레는 피가 청춘이 피고, 것이다. 이는 이상이 구하기 생생하며, 천하를 운다.',
-      nickname: 'Ann',
+      id: "1",
+      amount: "test1",
+      data: "사랑의 앞이 튼튼하며, 거친 사막이다. 청춘의 보배를 기쁘며, 날카로우나 구하지 하여도 그러므로 뿐이다. 이상 무엇을 목숨을 그들에게 천하를 능히 위하여, 그들은 듣기만 부패뿐이다. 내는 오직 실로 두손을 봄바람이다. 어디 무엇이 소금이라 있으며, 예가 기관과 인류의 뿐이다. 풀이 청춘의 지혜는 창공에 인간은 때까지 봄바람이다. 인류의 피는 주며, 자신과 쓸쓸하랴? 돋고, 그들의 것은 위하여, 그와 위하여서. 수 웅대한 설레는 피가 청춘이 피고, 것이다. 이는 이상이 구하기 생생하며, 천하를 운다.",
+      nickname: "Ann",
     },
     {
-      id: '2',
-      amount: 'test2',
-      data: 'bbbbbbbbbbbbbbb',
-      nickname: 'Tree',
+      id: "2",
+      amount: "test2",
+      data: "bbbbbbbbbbbbbbb",
+      nickname: "Tree",
     },
     {
-      id: '3',
-      amount: 'test3',
-      data: 'ccccccccccccccccccccc',
-      nickname: 'Lotto',
+      id: "3",
+      amount: "test3",
+      data: "ccccccccccccccccccccc",
+      nickname: "Lotto",
     },
   ]);
 
@@ -45,9 +45,9 @@ const Header = () => {
 
   // return dom 그려질때. 추적하는 상태가 바뀔때.
   useEffect(() => {
-    setHeader('👩🏻‍🦰');
+    setHeader("👩🏻‍🦰");
     // NOTE 로그인 이모지
-    console.log('header State', header);
+    console.log("header State", header);
   }, [header]);
 
   return (
@@ -71,17 +71,30 @@ const Header = () => {
             <Link to="/">공지사항</Link>
           </li>
 
-          <li>
-            <p onClick={showModal}>🔔</p>
-            {modalOpen &&
-              user.map(function (a) {
-                return <div key={user.id}>{user.amount}</div>;
-              })}
+          <li className="popover-link">
+            <span onClick={showPopover}>
+              <span className="bell">
+                <AiFillBell size={24} color="#2f92ff" />
+              </span>
+              {popover && (
+                <div className="popover">
+                  {user.map((item, index) => (
+                    <div key={index}>{item.nickname}</div>
+                  ))}
+                </div>
+              )}
+            </span>
+            {
+              //   popover &&
+              //   user.map((a, index) {
+              //     return <div key={a.id}>{a.amount}</div>;
+              // })
+            }
             {/* FIXME Key도 넣었는데 이게 왜 오류가 날까요? */}
           </li>
           <li>
             <Link to="/mypage">
-              <p>{header && header}</p>
+              <span>{header && header}</span>
             </Link>
           </li>
         </ul>
--- src/pages/mypage/Mypage.js
+++ src/pages/mypage/Mypage.js
@@ -1,68 +1,68 @@
-import classNames from 'classnames';
-import Left from 'components/left/Left';
-import { useSelector } from 'react-redux';
-import { Children, useState } from 'react';
-import './Mypage.scss';
+import classNames from "classnames";
+import Left from "components/left/Left";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+import "./Mypage.scss";
 
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from "react-router-dom";
 
-const Mypage = ({ user }) => {
+const Mypage = () => {
   const location = useLocation();
   const auth = useSelector((state) => state.authToken);
-  console.log('auth:', auth);
+  console.log("auth:", auth.accessToken);
 
   const contacts = [
-    { id: 0, type: '최근활동', line: '최근 활동 리스트들' },
-    { id: 1, type: '내가 쓴 글', line: '최근 내가 쓴 글들' },
-    { id: 2, type: '댓글', line: '최근 댓글' },
-    { id: 3, type: '스크랩', line: '스크랩 한 글들' },
+    { id: 0, type: "최근활동", line: "최근 활동 리스트들" },
+    { id: 1, type: "내가 쓴 글", line: "최근 내가 쓴 글들" },
+    { id: 2, type: "댓글", line: "최근 댓글" },
+    { id: 3, type: "스크랩", line: "스크랩 한 글들" },
   ];
-  const [select, setSelect] = useState('');
+  const [select, setSelect] = useState("");
   const onSelect = (type) => {
     setSelect(type);
   };
 
   const LoginRegist = () => {
-    <>
-      <Link
-        to="/login"
-        className={classNames('', {
-          'is-active': location.pathname === '/login',
-        })}
-      >
-        로그인
-      </Link>
-      {' | '}
-      <Link
-        to="/regist"
-        className={classNames('', {
-          'is-active': location.pathname === '/regist',
-        })}
-      >
-        회원가입
-      </Link>
-    </>;
+    return (
+      <div>
+        <Link
+          to="/login"
+          className={classNames("", {
+            "is-active": location.pathname === "/login",
+          })}
+        >
+          로그인
+        </Link>
+        {" | "}
+        <Link
+          to="/regist"
+          className={classNames("", {
+            "is-active": location.pathname === "/regist",
+          })}
+        >
+          회원가입
+        </Link>
+      </div>
+    );
   };
 
   return (
     <main className="main">
-      {{ user } ? '로그아웃' : <LoginRegist />}
-
+      {auth ? "로그아웃" : <LoginRegist />}
       <Left />
-
       <section className="notes">
         <ul>
-          {contacts.map((contact) => (
+          {contacts.map((contact, index) => (
             <li>
               <button
                 key={contact.id}
-                onClick={() => onSelect(contact.type)}
-                className={`${select === contact.type ? 'select' : ''}`}
+                onClick={() => onSelect(index)}
+                className={`${select === index && "select"}`}
               >
                 {contact.type}
               </button>
-              <p className={`${select === contact.type ? 'selects' : ''}`}>
-                {contact.line}
+              <p className={`${select === contact.type ? "selects" : ""}`}>
+                {select === index && contacts[select].line}
               </p>
               {/* FIXME 이건 버튼 클릭 시 색 변경까진 했는데, 클릭하고 내용이 나왔으면하는데, 모든 내용이 한 번에 다 나오네요..하다 보니 난리 났네요. 문제점 마구 알려주세요! */}
             </li>
