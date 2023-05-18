443552616c13f10ece866d172840f2000d18f132
rudwnok123 <rudwnok123@naver.com>
2023-05-18 13:12:36 +0900

    Revert "메인에 탑5"
    This reverts commit a3f8df90ec97ce23b063bbf4f2d6f4f55c1653d6.

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,22 +1,7 @@
 import React from "react";
-import { useEffect } from "react";
 
 const Main = () => {
-
-  useEffect(() => {
-    
-  }, [])
-  
-  return (
-    <div className="main-page page">
-      <section>
-        <strong>추천수 베스트 5</strong>
-        <ul>
-
-        </ul>
-      </section>
-    </div>
-  );
+  return <div className="main-page page">메인페이지</div>;
 };
 
 export default Main;
