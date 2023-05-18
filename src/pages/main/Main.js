a3f8df90ec97ce23b063bbf4f2d6f4f55c1653d6
rudwnok123 <rudwnok123@naver.com>
2023-05-18 13:08:08 +0900

    메인에 탑5

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { useEffect } from "react";
 
 const Main = () => {
-  return <div className="main-page page">메인페이지</div>;
+
+  useEffect(() => {
+    
+  }, [])
+  
+  return (
+    <div className="main-page page">
+      <section>
+        <strong>추천수 베스트 5</strong>
+        <ul>
+
+        </ul>
+      </section>
+    </div>
+  );
 };
 
 export default Main;
