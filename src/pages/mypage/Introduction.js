a0941f919a6c94062b7b5ce31a3917e6617c11e6
ssing_7942 <djagmlwnn12@gmail.com>
2023-05-19 21:08:20 +0900

    로그인 창 수정

--- src/pages/mypage/Introduction.js
+++ src/pages/mypage/Introduction.js
@@ -5,7 +5,7 @@ import Button from 'components/button/Button';
 
 const Introduction = () => {
   const [inputValue, setInputValue] = useState(
-    `	<img src="https://img.shields.io/badge/HTML5-E34F26?style=flat&logo=HTML5&logoColor=white" />`
+    `<p>안녕하세요</p> <p>테그를 사용하여 글을 작성하실 수 있어요</p> <img src="https://img.shields.io/badge/HTML5-E34F26?style=flat&logo=HTML5&logoColor=white" />`
   );
   const [submittedValue, setSubmittedValue] = useState('');
   const handleChange = (e) => {
@@ -25,7 +25,6 @@ const Introduction = () => {
       <main>
         <Left />
         <div className="notewrap">
-          <p>안녕하세요</p>
           <hr />
           <div className="note">
             <p dangerouslySetInnerHTML={{ __html: submittedValue }}></p>
