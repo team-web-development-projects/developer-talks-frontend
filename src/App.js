90d3fbae0990398fc3968c7c7f094bb2b1d3a203
ssing_7942 <djagmlwnn12@gmail.com>
2023-05-19 21:07:34 +0900

    oauth 로그인 window창 수정, auth로그인 페이지 제작

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import axios from "axios";
-import Footer from "components/footer/Footer";
-import Header from "components/header/Header";
-import { ROOT_API } from "constants/api";
-import NotPage from "pages/NotPage";
-import BoardDetail from "pages/board/boardDetail/BoardDetail";
-import BoardList from "pages/board/boardList/BoardList";
-import BoardPost from "pages/board/boardPost/BoardPost";
-import BoardUpdate from "pages/board/boardUpdate/BoardUpdate";
-import Login from "pages/login/Login";
-import Main from "pages/main/Main";
-import Account from "pages/mypage/Account";
-import Introduction from "pages/mypage/Introduction";
-import Mypage from "pages/mypage/Mypage";
-import Regist from "pages/regist/Regist";
-import StudyRoom from "pages/studyRoom/studyRoomList/StudyRoom";
-import StudyRoomPost from "pages/studyRoom/studyRoomPost/StudyRoomPost";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import axios from 'axios';
+import Footer from 'components/footer/Footer';
+import Header from 'components/header/Header';
+import { ROOT_API } from 'constants/api';
+import NotPage from 'pages/NotPage';
+import BoardDetail from 'pages/board/boardDetail/BoardDetail';
+import BoardList from 'pages/board/boardList/BoardList';
+import BoardPost from 'pages/board/boardPost/BoardPost';
+import BoardUpdate from 'pages/board/boardUpdate/BoardUpdate';
+import Authlogin from 'pages/login/Authlogin';
+import Login from 'pages/login/Login';
+import Main from 'pages/main/Main';
+import Account from 'pages/mypage/Account';
+import Introduction from 'pages/mypage/Introduction';
+import Mypage from 'pages/mypage/Mypage';
+import Regist from 'pages/regist/Regist';
+import StudyRoom from 'pages/studyRoom/studyRoomList/StudyRoom';
+import StudyRoomPost from 'pages/studyRoom/studyRoomPost/StudyRoomPost';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   Outlet,
   Route,
   Routes,
   useLocation,
   useNavigate,
-} from "react-router-dom";
-import { SET_TOKEN } from "store/Auth";
-import { getCookieToken, setRefreshToken } from "store/Cookie";
-import { isDev } from "util/Util";
-import "./assets/style/index.scss";
+} from 'react-router-dom';
+import { SET_TOKEN } from 'store/Auth';
+import { getCookieToken, setRefreshToken } from 'store/Cookie';
+import { isDev } from 'util/Util';
+import './assets/style/index.scss';
 
 function App() {
   const navigate = useNavigate();
@@ -37,20 +38,20 @@ function App() {
 
   useEffect(() => {
     if (isDev) {
-      console.log("dev");
+      console.log('dev');
     } else {
-      console.log("prod");
+      console.log('prod');
     }
 
-    if (window.location.href.includes("accessToken")) {
-      const accessToken = window.location.href.split("accessToken=")[1];
+    if (window.location.href.includes('accessToken')) {
+      const accessToken = window.location.href.split('accessToken=')[1];
       const refreshToken = window.location.href
-        .split("accessToken=")[1]
-        .split("&refreshToken=")[0];
+        .split('accessToken=')[1]
+        .split('&refreshToken=')[0];
       dispatch(SET_TOKEN({ accessToken: accessToken }));
       setRefreshToken({ refreshToken: refreshToken });
-      console.log("토큰있음");
-      navigate("/", { replace: true });
+      console.log('토큰있음');
+      navigate('/', { replace: true });
     }
   }, [dispatch, navigate, location]);
 
@@ -60,16 +61,16 @@ function App() {
         .post(`${ROOT_API}/token/refresh`, {
           refreshToken: getCookieToken().refreshToken,
           headers: {
-            accept: "*/*",
-            "Content-Type": "application/json",
+            accept: '*/*',
+            'Content-Type': 'application/json',
           },
         })
         .then(function (response) {
-          console.log("재갱신 성공:", response);
+          console.log('재갱신 성공:', response);
           dispatch(SET_TOKEN({ accessToken: response.data.accessToken }));
         })
         .catch(function (error) {
-          console.log("재갱신 실패: ", error.response.data);
+          console.log('재갱신 실패: ', error.response.data);
         });
     }
   }, [auth.accessToken, dispatch]);
@@ -121,6 +122,7 @@ function App() {
           />
           <Route path="/regist" element={<Regist />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/authlogin" element={<Authlogin />} />
         </Route>
       </Routes>
     </div>
