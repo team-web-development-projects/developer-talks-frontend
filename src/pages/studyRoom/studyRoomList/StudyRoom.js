458f7658e74b3799faa5a84b5941af1e72112ade
rudwnok123 <rudwnok123@naver.com>
2023-05-28 00:08:50 +0900

    로그인한 사용자만 스터디룸

--- src/pages/studyRoom/studyRoomList/StudyRoom.js
+++ src/pages/studyRoom/studyRoomList/StudyRoom.js
@@ -65,67 +65,74 @@ const BoardList = ({ type }) => {
   if (isLoading) return <div>Loading...</div>;
 
   return (
-    <div className={s.studyroom}>
-      {modal && (
-        <BasicModal setOnModal={() => setModal()}>
-          로그인을 하면 게시글을 작성할 수 있어요.
-          <br />
-          <Link to="/login">[로그인 하러 가기]</Link>
-          <br />
-        </BasicModal>
-      )}
-      <div className={s.banner}>
-        <p>⭐스터디룸⭐</p>
-        <p>공부방</p>
-      </div>
-      <div className={s.header}>
-        <form className={s.search} onSubmit={handleSearch}>
-          <BiSearch />
-          <input type="text" placeholder="원하는 내용을 검색해보세요~!" />
-        </form>
-        <div className={s.bottom}>
-          <Select init="최신순" options={["최신순", "조회순"]} />
-          <Button onClick={handleClick}>✏️룸 만들기</Button>
-        </div>
-      </div>
-      <ul className={s.list}>
-        {data ? (
-          data.content.map((item, index) => (
-            <li
-              key={index}
-              className={s.card_list}
-              onClick={() => joinRoomClick(item.id)}
-            >
-              <div className={s.title}>{item.title}</div>
-              <div className={s.tag}>
-                {item.skills.map((items, indexs) => (
-                  <span key={indexs}>{items}</span>
-                ))}
-              </div>
-              <div className={s.info}>
-                <div className={s.maker}>{item.studyRoomUsers[0].nickname}</div>
-                <div className={s.icon}>
-                  <BsFillPeopleFill size={16} />
-                  <span>{item.studyRoomUsers.length}</span>/
-                  <span>{item.joinableCount}</span>
-                </div>
-              </div>
-            </li>
-          ))
-        ) : (
-          <li>등록된 게시글이 없습니다.</li>
-        )}
-      </ul>
-
-      <div className={s.pageContainer}>
-        <Pagination
-          currentPage={data.pageable.pageNumber + 1}
-          totalPage={data.totalPages}
-          paginate={setCurrentPage}
-        />
-      </div>
-      <Scrolltop />
-    </div>
+    <>
+      {auth.accessToken !== null ? 
+        <div className={s.studyroom}>
+          {modal && (
+            <BasicModal setOnModal={() => setModal()}>
+              로그인을 하면 게시글을 작성할 수 있어요.
+              <br />
+              <Link to="/login">[로그인 하러 가기]</Link>
+              <br />
+            </BasicModal>
+          )}
+          <div className={s.banner}>
+            <p>⭐스터디룸⭐</p>
+            <p>공부방</p>
+          </div>
+          <div className={s.header}>
+            <form className={s.search} onSubmit={handleSearch}>
+              <BiSearch />
+              <input type="text" placeholder="원하는 내용을 검색해보세요~!" />
+            </form>
+            <div className={s.bottom}>
+              <Select init="최신순" options={["최신순", "조회순"]} />
+              <Button onClick={handleClick}>✏️룸 만들기</Button>
+            </div>
+          </div>
+          <ul className={s.list}>
+            {data ? (
+              data.content.map((item, index) => (
+                <li
+                  key={index}
+                  className={s.card_list}
+                  onClick={() => joinRoomClick(item.id)}
+                >
+                  <div className={s.title}>{item.title}</div>
+                  <div className={s.tag}>
+                    {item.skills.map((items, indexs) => (
+                      <span key={indexs}>{items}</span>
+                    ))}
+                  </div>
+                  <div className={s.info}>
+                    <div className={s.maker}>
+                      {item.studyRoomUsers[0].nickname}
+                    </div>
+                    <div className={s.icon}>
+                      <BsFillPeopleFill size={16} />
+                      <span>{item.studyRoomUsers.length}</span>/
+                      <span>{item.joinableCount}</span>
+                    </div>
+                  </div>
+                </li>
+              ))
+            ) : (
+              <li>등록된 게시글이 없습니다.</li>
+            )}
+          </ul>
+  
+          <div className={s.pageContainer}>
+            <Pagination
+              currentPage={data.pageable.pageNumber + 1}
+              totalPage={data.totalPages}
+              paginate={setCurrentPage}
+            />
+          </div>
+          <Scrolltop />
+        </div> : 
+        <div>asdf</div>
+      }
+    </>
   );
 };
 
