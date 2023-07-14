9d142773dd3a4af60d866e3529daae9272912407
rudwnok123 <rudwnok123@naver.com>
2023-07-14 20:54:27 +0900

    임시 수정

--- src/pages/mypage/_com/userside/Userside.js
+++ src/pages/mypage/_com/userside/Userside.js
@@ -37,56 +37,53 @@ const Userside = () => {
       console.log("닉네임 클릭함");
       setViewSide(true);
     }
-    if (location.state && location.state.nickname === null) {
+    if (location.state && location.state.nickname === null && parseJwt(auth.accessToken).nickname !== null) {
       // 프로필 클릭
       console.log("프로필 클릭함");
       setViewSide(true);
     }
-  }, [auth.accessToken, location.state]);
+  }, [location.state]);
 
-  console.log("닉: ", location.state && location.state.nickname, parseJwt(auth.accessToken).nickname
-  )
+  console.log("닉: ", location.state && location.state.nickname, parseJwt(auth.accessToken).nickname);
 
   return (
     <MypageContent>
-    {viewSide ? 'true' : 'false'}
+      {viewSide ? "true" : "false"}
       {
         // 게시글을 통해 들어온 닉네임과 내 닉네임이 같을때
-        viewSide && (
-          // 게시글을 통해 들어오지 않을때
-          <section className="side">
-            <div className="imgwrap">
-              <ProfileImg size="big" />
-            </div>
-            <ul className="nav">
-              <li
-                className={classNames("", { "is-active": isActive === "mypage" })}
-                onClick={() => handleClick("mypage")}
-              >
-                활동내역
-              </li>
-              <li
-                className={classNames("", { "is-active": isActive === "my-studyroom" })}
-                onClick={() => handleClick("my-studyroom")}
-              >
-                스터디룸
-              </li>
-              <li
-                className={classNames("", { "is-active": isActive === "my-message" })}
-                onClick={() => handleClick("my-message")}
-              >
-                쪽지
-              </li>
-              <li
-                className={classNames("", { "is-active": isActive === "account" })}
-                onClick={() => handleClick("account")}
-              >
-                회원정보수정 및 탈퇴
-              </li>
-            </ul>
-            <Logout />
-          </section>
-        )
+        // 게시글을 통해 들어오지 않을때
+        <section className="side">
+          <div className="imgwrap">
+            <ProfileImg size="big" />
+          </div>
+          <ul className="nav">
+            <li
+              className={classNames("", { "is-active": isActive === "mypage" })}
+              onClick={() => handleClick("mypage")}
+            >
+              활동내역
+            </li>
+            <li
+              className={classNames("", { "is-active": isActive === "my-studyroom" })}
+              onClick={() => handleClick("my-studyroom")}
+            >
+              스터디룸
+            </li>
+            <li
+              className={classNames("", { "is-active": isActive === "my-message" })}
+              onClick={() => handleClick("my-message")}
+            >
+              쪽지
+            </li>
+            <li
+              className={classNames("", { "is-active": isActive === "account" })}
+              onClick={() => handleClick("account")}
+            >
+              회원정보수정 및 탈퇴
+            </li>
+          </ul>
+          <Logout />
+        </section>
       }
       <>{isActive === "mypage" && <UserInfoList user={location.state} />}</>
       <>{isActive === "my-studyroom" && <MyStudyRoom />}</>
