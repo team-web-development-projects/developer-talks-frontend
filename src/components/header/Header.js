74f4acb907dbfb55eea85f7821dc44a2f77f1fb3
ssing_7942 <djagmlwnn12@gmail.com>
2023-07-22 16:14:57 +0900

    헤더 메뉴바 반응형

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,6 +10,7 @@ import { useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 import "./header.scss";
 import { useOutOfClick } from "hooks/useOutOfClick";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const Header = () => {
   const auth = useSelector((state) => state.authToken);
@@ -19,7 +20,10 @@ const Header = () => {
   let nickname = "";
   const targetRef = useRef(null);
   const location = useLocation();
-
+  const [toggleShow, setToggleShow] = useState(false);
+    useEffect(() => {
+      setToggleShow(false);
+    }, [location]);
   const showPopover = () => {
     setPopover(!popover);
   };
@@ -50,51 +54,49 @@ const Header = () => {
   useOutOfClick(targetRef, () => {
     setPopover(false);
   });
-
+  const visible = () => {
+    setToggleShow(!toggleShow);
+  };
   // console.log('헤더 노티 : ', noti)
 
   return (
     <header className="header">
-      <div className="header-wrap">
-        <Link className="logo" to="/">
-          Developer-Talks
-        </Link>
-        <nav className="navBar">
-          <ul className="right">
-            {menuRouter.map((item, i) => (
-              <li key={i}>
-                <Link
-                  to={item.link}
-                  className={classNames("", {
-                    "is-active": location.pathname.includes(item.link),
-                  })}
-                >
-                  {item.text}
-                </Link>
-              </li>
-            ))}
-            <li className="popover-link">
-              <span onClick={showPopover} ref={targetRef}>
-                <span className="bell">
-                  {/* TODO: 알람이 있을때 표시하기 */}
-                  {noti.noti && auth.accessToken !== null && <span className="point" />}
-                  <TfiBell size={24} />
-                </span>
-                <Notification classname={popover ? "is_show" : ""} />
-              </span>
-            </li>
-            <li className="header-user">
-              <Link to="/showuser">{!nickname ? <BsFillPersonFill size={24} /> : <ProfileImg border="color" type="header" />}</Link>
-              {nickname && <span>{`${nickname}님`}</span>}
-            </li>
-          </ul>
-        </nav>
-        <div className="menuBar">
-          <Link to="/">
-            <FiMenu size={24} />
-          </Link>
-        </div>
-      </div>
+      <Link className="logo" to="/">
+        Developer-Talks
+      </Link>
+      <ul className={`navBar ${toggleShow ? "open" : "notOpen"}`}>
+        {menuRouter.map((item, i) => (
+          <li key={i}>
+            <Link
+              to={item.link}
+              className={classNames("", {
+                "is-active": location.pathname.includes(item.link),
+              })}
+            >
+              {item.text}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <ul className="menuBar">
+        <li className="popover-link">
+          <span onClick={showPopover} ref={targetRef}>
+            <span className="bell">
+              {/* TODO: 알람이 있을때 표시하기 */}
+              {noti.noti && auth.accessToken !== null && <span className="point" />}
+              <TfiBell size={24} />
+            </span>
+            <Notification classname={popover ? "is_show" : ""} />
+          </span>
+        </li>
+        <li className="header-user">
+          <Link to="/showuser">{!nickname ? <BsFillPersonFill size={24} /> : <ProfileImg border="color" type="header" />}</Link>
+          {nickname && <span>{`${nickname}님`}</span>}
+        </li>
+        <li className="display">
+          <AiOutlineMenu size={26} onClick={visible} className={`${toggleShow ? "showMenu" : "notShowMenu"}`} />
+        </li>
+      </ul>
     </header>
   );
 };
