70650f3c185d5d49d043c7e5041074c555059918
rudwnok123 <rudwnok123@naver.com>
2023-07-30 11:35:29 +0900

    헤더 수정

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,7 +10,7 @@ import { useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 import "./header.scss";
 import { useOutOfClick } from "hooks/useOutOfClick";
-import { AiOutlineClose } from 'react-icons/ai';
+import { AiOutlineClose } from "react-icons/ai";
 const Header = () => {
   const auth = useSelector((state) => state.authToken);
   const noti = useSelector((state) => state.notification);
@@ -64,10 +64,10 @@ const Header = () => {
         <Link className="logo" to="/">
           Developer-Talks
         </Link>
-        <nav className="navBar">
+        <nav>
           <ul className="right">
             {menuRouter.map((item, i) => (
-              <li key={i} className="hidden">
+              <li key={i}>
                 <Link
                   to={item.link}
                   className={classNames("", {
@@ -89,30 +89,40 @@ const Header = () => {
               </span>
             </li>
             <li className="header-user">
-              <Link to="/showuser">{!nickname ? <BsFillPersonFill size={24} /> : <ProfileImg border="color" type="header" />}</Link>
+              <Link to="/showuser">
+                {!nickname ? <BsFillPersonFill size={24} /> : <ProfileImg border="color" type="header" />}
+              </Link>
               {nickname && <span>{`${nickname}님`}</span>}
             </li>
           </ul>
         </nav>
-        <div onClick={visible} className={`menuBar ${ toggleShow ? "showMenu" : "notShowMenu"}`} >
-          {toggleShow ? <FiMenu size={24} /> : <AiOutlineClose size={24} />}
+        <div onClick={visible} className={`mobile-menu ${toggleShow && "is-show"}`}>
+          {toggleShow ? <AiOutlineClose size={24} /> : <FiMenu size={24} />}
         </div>
       </div>
 
-      <ul className={`moBar ${toggleShow ? "open" : "notOpen"}`}>
-        {menuRouter.map((item, i) => (
-          <li key={i}>
-            <Link
-              to={item.link}
-              className={classNames("", {
-                "is-active": location.pathname.includes(item.link),
-              })}
-            >
-              {item.text}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <div className={`mobile-nav ${toggleShow && "is-open"}`}>
+        <div className="header-user">
+          <Link to="/showuser">
+            {!nickname ? <BsFillPersonFill size={24} /> : <ProfileImg border="color" type="header" className="profile"/>}
+          </Link>
+          {nickname && <span>{`${nickname}님`}</span>}
+        </div>
+        <ul>
+          {menuRouter.map((item, i) => (
+            <li key={i}>
+              <Link
+                to={item.link}
+                className={classNames("", {
+                  "is-active": location.pathname.includes(item.link),
+                })}
+              >
+                {item.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </header>
   );
 };
